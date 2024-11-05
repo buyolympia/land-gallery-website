c7083ade1cb8ad0cad2bbabcaa8d0700d3f6f70c
Pat Castaldo <pat@buyolympia.com>
2024-11-05 12:25:05 -0800

    This is me checking in the latest, still figuring out how the images plugin works to resize and do everytrhing right… I am just gonna copy the output to S3 for now.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 
 module.exports = function(eleventyConfig) {
 
@@ -12,9 +13,28 @@ module.exports = function(eleventyConfig) {
 	
 	//add css cleanup 
 	const CleanCSS = require("clean-css");
-
 	eleventyConfig.addFilter("cssmin", function (code) {
 		return new CleanCSS({}).minify(code).styles;
+	});
+	
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		// which file extensions to process
+		extensions: "liquid",
+	
+		// Add any other Image utility options here:
+	
+		// optional, output image formats
+		formats: ["webp", "jpeg","jpg"],
+		// formats: ["auto"],
+	
+		// optional, output image widths
+		 widths: ["auto"],
+	
+		// optional, attributes assigned on <img> override these values.
+		defaultAttributes: {
+			loading: "lazy",
+			decoding: "async",
+		},
 	});
 	
 	//put the date on the footer?
